126881df0c481bef62254d4a3d27bddc9e084dd7
ZHENGMINGTI <1714539508@qq.com>
2023-03-12 10:00:03 +0800

    hello

---
+++ D/src/test1/circle.java
@@ -0,0 +1,15 @@
+package test1;
+
+public class circle {
+	int radius;
+	double area;
+
+
+	public static void main(String[] args) {
+		circle x;
+		x=new circle();
+		System.out.print(x);
+		System.out.println("radius="+x.radius);
+		System.out.println("area"=MATH)
+	}
+}
